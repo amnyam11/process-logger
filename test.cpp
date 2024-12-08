4cef35bf56f7dda0dfc3f9d8393ff04e308407bc
amnyam11 <mishinandrey851@gmail.com>
2024-12-08 23:28:50 +1000

    added timer on counter and log

--- test.cpp
+++ test.cpp
@@ -4,7 +4,13 @@
 #include <string>
 #include <sstream>
 
-// Имя разделяемой памяти и семафора
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
 const char* MEM_NAME = "counter_mem";
 const char* LOG_FILE_NAME = "log_file.log";
 
@@ -16,6 +22,34 @@ struct my_data
     int counter;
     int pid_main_process;
 };
+
+void sleep_ms(int milliseconds) {
+#ifdef WIN32
+    Sleep(milliseconds);
+#else
+    usleep(milliseconds * 1000);
+#endif
+}
+
+
+unsigned long long get_current_time_ms() {
+#ifdef WIN32
+    return GetTickCount();
+#else
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000ULL + tv.tv_usec / 1000ULL;
+#endif
+}
+
+
+int getCurrentPID(){
+#ifdef WIN32
+    return GetCurrentProcessId();
+#else
+    return getpid();
+#endif
+}
 
 std::string getCurrentTime() {
     std::ostringstream oss;
@@ -39,17 +73,33 @@ std::string getCurrentTime() {
     return oss.str();
 }
 
+void incrementCounter(cplib::SharedMem<my_data>& shared_data) {
+    shared_data.Lock();
+    shared_data.Data()->counter++;
+    shared_data.Unlock();
+}
 
-int getCurrentPID(){
-#ifdef WIN32
-    return GetCurrentProcessId();
-#else
-    return getpid();
-#endif
+
+void writeToLog(const std::string& message) {
+    std::ofstream logFile(LOG_FILE_NAME, std::ios::app);
+    if (logFile.is_open()) {
+        logFile << message << std::endl;
+        logFile.close();
+    } else {
+        std::cerr << "Failed to open log file." << std::endl;
+    }
+}
+
+void logCounter(cplib::SharedMem<my_data>& shared_data) {
+    shared_data.Lock();
+    std::string currentTime = getCurrentTime();
+    int pid = getCurrentPID();
+    writeToLog(currentTime + " PID: " + std::to_string(pid) + " Counter: " + std::to_string(shared_data.Data()->counter));
+    shared_data.Unlock();
 }
 
 int main() {
-    // Создаем или открываем разделяемую память для хранения counter
+
     cplib::SharedMem<my_data> shared_data(MEM_NAME);
 
     if (!shared_data.IsValid()) {
@@ -57,44 +107,40 @@ int main() {
         return 1;
     }
 
+
     my_data* data = shared_data.Data();
+
     std::string command;
 
+    int pid = getCurrentPID();
+    if (pid == data->pid_main_process)
+        writeToLog(getCurrentTime() + " PID: " + std::to_string(pid) + " Start working.");
+
+    unsigned long long lastIncrementTime = get_current_time_ms();
+    unsigned long long lastLogTime = get_current_time_ms();
+   
     while (true) {
-        std::cin >> command;
-        if (command == "s") {
-            // Блокируем разделяемую память для чтения counter
-            shared_data.Lock();
-            std::cout << "Counter: " << data->counter << std::endl;
-            shared_data.Unlock();
-        } else if (command == "m") {
-            // Блокируем разделяемую память для изменения counter
-            shared_data.Lock();
-            data->counter++;
-            std::cout << "Counter incremented. New value: " << data->counter << std::endl;
-            shared_data.Unlock();
-        } else if (command == "e") {
-            break;
-        } else if (command == "l") {
-            int pid = getCurrentPID();
-            // Блокируем разделяемую память для записи в лог-файл
-            shared_data.Lock();
-            if (data->pid_main_process == pid) {
-                std::ofstream logFile(LOG_FILE_NAME, std::ios::app);
-                if (logFile.is_open()) {
-                    std::string currentTime = getCurrentTime();
-                    logFile << currentTime << " PID: " << pid << " Counter: " << data->counter << std::endl;
-                    logFile.close();
-                } else {
-                    std::cerr << "Failed to open log file." << std::endl;
-                }
-            } else {
-                std::cout << "Only the main program can write to the log file." << std::endl;
+
+        unsigned long long currentTime = get_current_time_ms();
+
+        if (currentTime - lastIncrementTime >= 300) {
+            lastIncrementTime = currentTime;
+
+            if (pid == data->pid_main_process) {
+                shared_data.Lock();
+                data->counter++;
+                shared_data.Unlock();
             }
-            shared_data.Unlock();
-        } else {
-            std::cout << "unknown param! Use 's' to show or 'm' to modify!" << std::endl;
         }
+        if (currentTime - lastLogTime >= 1000){
+            lastLogTime = currentTime;
+            if (pid == data->pid_main_process) {
+                logCounter(shared_data);
+            }
+        } 
+
+        sleep_ms(10);
+
     }
 
     return 0;
